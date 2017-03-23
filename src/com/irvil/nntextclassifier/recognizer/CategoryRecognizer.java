aa696e2a3895d3ad936f70322e9d0ef7a62ced36
Ruslan Zakaryaev <irvilcom@gmail.com>
2017-03-23 17:21:41 +0400

    Add Category and Handler recognizers

---
+++ src/com/irvil/nntextclassifier/recognizer/CategoryRecognizer.java
@@ -0,0 +1,34 @@
+package com.irvil.nntextclassifier.recognizer;
+
+import com.irvil.nntextclassifier.dao.CategoryDAO;
+import com.irvil.nntextclassifier.dao.jdbc.JDBCCategoryDAO;
+import com.irvil.nntextclassifier.model.Category;
+import com.irvil.nntextclassifier.model.IncomingCall;
+
+// todo: check in translate
+public class CategoryRecognizer extends Recognizer<Category> {
+  private CategoryDAO categoryDAO;
+
+  @Override
+  protected int getOutputLayerSize() {
+    initializeDAO();
+    return categoryDAO.getCount();
+  }
+
+  @Override
+  protected Category convertVectorToValue(double[] output) {
+    initializeDAO();
+    return categoryDAO.findByVector(output);
+  }
+
+  @Override
+  protected double[] getCatalogValueVector(IncomingCall incomingCall) {
+    return incomingCall.getModule().asVector();
+  }
+
+  private void initializeDAO() {
+    if (categoryDAO == null) {
+      categoryDAO = new JDBCCategoryDAO();
+    }
+  }
+}
---
+++ src/com/irvil/nntextclassifier/recognizer/HandlerRecognizer.java
@@ -0,0 +1,34 @@
+package com.irvil.nntextclassifier.recognizer;
+
+import com.irvil.nntextclassifier.dao.HandlerDAO;
+import com.irvil.nntextclassifier.dao.jdbc.JDBCHandlerDAO;
+import com.irvil.nntextclassifier.model.Handler;
+import com.irvil.nntextclassifier.model.IncomingCall;
+
+// todo: check in translate
+public class HandlerRecognizer extends Recognizer<Handler> {
+  private HandlerDAO handlerDAO;
+
+  @Override
+  protected int getOutputLayerSize() {
+    initializeDAO();
+    return handlerDAO.getCount();
+  }
+
+  @Override
+  protected Handler convertVectorToValue(double[] output) {
+    initializeDAO();
+    return handlerDAO.findByVector(output);
+  }
+
+  @Override
+  protected double[] getCatalogValueVector(IncomingCall incomingCall) {
+    return incomingCall.getModule().asVector();
+  }
+
+  private void initializeDAO() {
+    if (handlerDAO == null) {
+      handlerDAO = new JDBCHandlerDAO();
+    }
+  }
+}
