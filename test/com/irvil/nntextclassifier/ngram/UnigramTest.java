58408f34d3d6f6b8e1a47e16638e43f97d2ed9e3
Ruslan Zakaryaev <irvilcom@gmail.com>
2017-05-22 13:06:46 +0400

    NGramStrategy tests refactoring

--- test/com/irvil/nntextclassifier/ngram/UnigramTest.java
+++ test/com/irvil/nntextclassifier/ngram/UnigramTest.java
@@ -1,45 +1,10 @@
 package com.irvil.nntextclassifier.ngram;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.Set;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-public class UnigramTest {
-  private NGramStrategy nGramStrategy;
-  private Set<String> uniqueValues;
-
-  @Before
+public class UnigramTest extends NGramStrategyTest {
+  @Override
   public void setUp() {
     nGramStrategy = new Unigram();
-  }
-
-  @Test
-  public void getNGramCyrillicText() throws Exception {
-    uniqueValues = nGramStrategy.getNGram("Привет. Хотела бы сделать 235.2 тест метода. Тест,.. Хотел сделал.");
-    String[] ideal = {"привет", "хотела", "бы", "сделать", "235", "2", "тест", "метода", "хотел", "сделал"};
-
-    assertArrayEquals(ideal, uniqueValues.toArray());
-  }
-
-  @Test
-  public void getNGramLatinText() throws Exception {
-    uniqueValues = nGramStrategy.getNGram("Hello! This is method 23 test. Test methods hello");
-    String[] ideal = {"hello", "this", "is", "method", "23", "test", "methods"};
-
-    assertArrayEquals(ideal, uniqueValues.toArray());
-  }
-
-  @Test
-  public void getNGramEmptyString() throws Exception {
-    assertEquals(nGramStrategy.getNGram("").size(), 0);
-  }
-
-  @Test
-  public void getNGramNull() throws Exception {
-    assertEquals(nGramStrategy.getNGram(null).size(), 0);
+    idealCyrillicText = new String[] {"привет", "хотела", "бы", "сделать", "235", "2", "тест", "метода", "хотел", "сделал"};
+    idealLatinText = new String[] {"hello", "this", "is", "method", "23", "test", "methods"};
   }
 }
