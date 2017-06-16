b38094b7d36b5668a0146722c6dd580e52e4277e
Ruslan Zakaryaev <irvilcom@gmail.com>
2017-06-16 15:33:18 +0400

    Change vocabulary creation algorithm.

--- src/com/irvil/nntextclassifier/FirstStart.java
+++ src/com/irvil/nntextclassifier/FirstStart.java
@@ -181,22 +181,33 @@ class FirstStart implements Observable {
     }
   }
 
+  // todo: move to VocabularyBuilder class
   private List<VocabularyWord> getVocabularyFromIncomingCallsTexts(List<IncomingCall> incomingCalls) {
-    Set<String> uniqueValues = new LinkedHashSet<>();
+    Map<String, Integer> uniqueValues = new HashMap<>();
     List<VocabularyWord> vocabulary = new ArrayList<>();
 
-    // add words (converted to n-gram) from all Incoming Calls Texts to vocabulary
+    // count frequency of use each word (converted to n-gram) from all Incoming Calls Texts
     //
 
     for (IncomingCall incomingCall : incomingCalls) {
-      uniqueValues.addAll(nGram.getNGram(incomingCall.getText()));
+      for (String word : nGram.getNGram(incomingCall.getText())) {
+        if (uniqueValues.containsKey(word)) {
+          // increase counter
+          uniqueValues.put(word, uniqueValues.get(word) + 1);
+        } else {
+          // add new word
+          uniqueValues.put(word, 1);
+        }
+      }
     }
 
-    // convert uniqueValues to Vocabulary
+    // convert uniqueValues to Vocabulary, excluding rare (frequency of use less then 4)
     //
 
-    for (String uniqueValue : uniqueValues) {
-      vocabulary.add(new VocabularyWord(uniqueValue));
+    for (Map.Entry<String, Integer> entry : uniqueValues.entrySet()) {
+      if (entry.getValue() > 3) {
+        vocabulary.add(new VocabularyWord(entry.getKey()));
+      }
     }
 
     return vocabulary;
