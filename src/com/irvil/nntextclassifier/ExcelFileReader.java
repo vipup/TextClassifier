8a81e5932196698dd2ec2686cee977b195c22f0e
Ruslan Zakaryaev <irvilcom@gmail.com>
2017-06-23 13:21:31 +0400

    Refactoring.

--- src/com/irvil/nntextclassifier/ExcelFileReader.java
+++ src/com/irvil/nntextclassifier/ExcelFileReader.java
@@ -3,6 +3,7 @@ package com.irvil.nntextclassifier;
 import com.irvil.nntextclassifier.model.Characteristic;
 import com.irvil.nntextclassifier.model.CharacteristicValue;
 import com.irvil.nntextclassifier.model.ClassifiableText;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -20,39 +21,46 @@ class ExcelFileReader {
       throw new IllegalArgumentException();
     }
 
+    try (XSSFWorkbook excelFile = new XSSFWorkbook(new FileInputStream(xlsxFile))) {
+      return getClassifiableTexts(excelFile.getSheetAt(0));
+    }
+  }
+
+  private List<ClassifiableText> getClassifiableTexts(XSSFSheet sheet) {
+    List<Characteristic> characteristics = getCharacteristics(sheet);
     List<ClassifiableText> classifiableTexts = new ArrayList<>();
 
-    try (XSSFWorkbook excelFile = new XSSFWorkbook(new FileInputStream(xlsxFile))) {
-      XSSFSheet sheet = excelFile.getSheetAt(0);
+    // start from second row
+    for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+      Map<Characteristic, CharacteristicValue> characteristicsValues = getCharacteristicsValues(sheet.getRow(i), characteristics);
 
-      // create Characteristics catalog
-      // first row contains Characteristics names from second to last columns
-      //
-
-      List<Characteristic> characteristics = new ArrayList<>();
-
-      for (int i = 1; i < sheet.getRow(0).getLastCellNum(); i++) {
-        characteristics.add(new Characteristic(sheet.getRow(0).getCell(i).getStringCellValue()));
+      // exclude empty rows
+      if (!sheet.getRow(i).getCell(0).getStringCellValue().equals("")) {
+        classifiableTexts.add(new ClassifiableText(sheet.getRow(i).getCell(0).getStringCellValue(), characteristicsValues));
       }
-
-      // fill ClassifiableTexts
-      // start from second row
-      //
-
-      for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-        Map<Characteristic, CharacteristicValue> characteristicsValues = new HashMap<>();
-
-        for (int j = 1; j < sheet.getRow(i).getLastCellNum(); j++) {
-          characteristicsValues.put(characteristics.get(j - 1), new CharacteristicValue(sheet.getRow(i).getCell(j).getStringCellValue()));
-        }
-
-        // exclude empty rows
-        if (!sheet.getRow(i).getCell(0).getStringCellValue().equals("")) {
-          classifiableTexts.add(new ClassifiableText(sheet.getRow(i).getCell(0).getStringCellValue(), characteristicsValues));
-        }
-      }
-
-      return classifiableTexts;
     }
+
+    return classifiableTexts;
+  }
+
+  private Map<Characteristic, CharacteristicValue> getCharacteristicsValues(Row row, List<Characteristic> characteristics) {
+    Map<Characteristic, CharacteristicValue> characteristicsValues = new HashMap<>();
+
+    for (int i = 1; i < row.getLastCellNum(); i++) {
+      characteristicsValues.put(characteristics.get(i - 1), new CharacteristicValue(row.getCell(i).getStringCellValue()));
+    }
+
+    return characteristicsValues;
+  }
+
+  private List<Characteristic> getCharacteristics(XSSFSheet sheet) {
+    List<Characteristic> characteristics = new ArrayList<>();
+
+    // first row from second to last columns contains Characteristics names
+    for (int i = 1; i < sheet.getRow(0).getLastCellNum(); i++) {
+      characteristics.add(new Characteristic(sheet.getRow(0).getCell(i).getStringCellValue()));
+    }
+
+    return characteristics;
   }
 }
